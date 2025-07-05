44f7fcc8d87dde6a8bb58ba6cd447f26588fb9c7
Liam Kinne <liam@kinne.io>
2025-07-06 00:13:35 +1000

    remove whitespace

--- messages/src/lib.rs
+++ messages/src/lib.rs
@@ -8,18 +8,13 @@ use j1939::Pgn;
 
 /// Output control messages.
 pub const CONTROL: Pgn = Pgn::ProprietaryA;
-
 /// Configuration messages.
 pub const CONFIGURE: Pgn = Pgn::ProprietaryA2;
-
 /// Power-on reset message.
 pub const STARTUP: Pgn = Pgn::ProprietaryB(0x00);
-
 /// System status message.
 pub const SYSTEM_STATUS: Pgn = Pgn::ProprietaryB(0x12);
-
 /// Current sense message.
 pub const CURRENT_SENSE: Pgn = Pgn::ProprietaryB(0x10);
-
 /// Analog readings message.
 pub const ANALOG_READINGS: Pgn = Pgn::ProprietaryB(0x11);
