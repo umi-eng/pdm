08c16e8457ec041d956072bd1c1074c5b08289e3
Liam Kinne <liam@kinne.io>
2025-07-07 04:25:20 +1000

    fix args

--- pdm/examples/update_firmware.rs
+++ pdm/examples/update_firmware.rs
@@ -6,10 +6,10 @@ use std::io::{self, Read};
 async fn main() -> Result<(), io::Error> {
     let args: Vec<String> = std::env::args().collect();
 
-    let socket = CanSocket::open(&args[0])?;
+    let socket = CanSocket::open(&args[1])?;
     let pdm = pdm36::Pdm36::new(socket, 0x55);
 
-    let mut file = std::fs::File::open(&args[1])?;
+    let mut file = std::fs::File::open(&args[2])?;
     let mut firmware = vec![];
     file.read_to_end(&mut firmware)?;
 
