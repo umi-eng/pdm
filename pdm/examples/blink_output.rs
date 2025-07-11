3f17525db6daa5101aa7b196c7b06c024c9f0ece
Liam Kinne <liam@kinne.io>
2025-07-12 07:19:14 +1000

    add output control example

---
+++ pdm/examples/blink_output.rs
@@ -0,0 +1,32 @@
+//! Blink an output at 0.5Hz.
+//!
+//! Usage: cargo run --example blink_output <INTERFACE> <ADDRESS> <OUTPUT>
+//! Example: cargo run --example blink_output can0 85 1
+
+use pdm::pdm36;
+use socketcan::tokio::CanSocket;
+use std::{
+    io::{self, Read},
+    time::Duration,
+};
+use tokio::time::sleep;
+
+#[tokio::main]
+async fn main() -> Result<(), io::Error> {
+    tracing_subscriber::fmt::init();
+
+    let args: Vec<String> = std::env::args().collect();
+    let interface = &args[1];
+    let address: u8 = args[2].parse().unwrap();
+    let output: usize = args[2].parse().unwrap();
+
+    let socket = CanSocket::open(interface)?;
+    let mut pdm = pdm36::Pdm36::new(socket, address);
+
+    loop {
+        pdm.set_output(output, true).await?;
+        sleep(Duration::from_secs(1)).await;
+        pdm.set_output(output, false).await?;
+        sleep(Duration::from_secs(1)).await;
+    }
+}
