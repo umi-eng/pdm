97ce864b0873635cac0bfe2c095d4f11fc595595
Liam Kinne <liam@kinne.io>
2025-07-13 20:58:44 +1000

    sample many times per analog input message

--- pdm-36/src/analog.rs
+++ pdm-36/src/analog.rs
@@ -48,17 +48,23 @@ pub async fn analog(cx: analog::Context<'_>) {
     Mono::delay(100_u64.millis()).await;
 
     loop {
-        let start = Mono::now();
+        // sample many times
+        for _ in 0..100 {
+            let start = Mono::now();
 
-        // read inputs
-        let ain_1 = count_to_volts(VREF, MAX_COUNT as f32, adc_4.blocking_read(ain_1) as f32);
-        let ain_2 = count_to_volts(VREF, MAX_COUNT as f32, adc_3.blocking_read(ain_2) as f32);
-        let ain_3 = count_to_volts(VREF, MAX_COUNT as f32, adc_4.blocking_read(ain_3) as f32);
+            // read inputs
+            let ain_1 = count_to_volts(VREF, MAX_COUNT as f32, adc_4.blocking_read(ain_1) as f32);
+            let ain_2 = count_to_volts(VREF, MAX_COUNT as f32, adc_3.blocking_read(ain_2) as f32);
+            let ain_3 = count_to_volts(VREF, MAX_COUNT as f32, adc_4.blocking_read(ain_3) as f32);
 
-        // get adc readings
-        ain_1_avg.push(divider_vin(R1, R2, ain_1));
-        ain_2_avg.push(divider_vin(R1, R2, ain_2));
-        ain_3_avg.push(divider_vin(R1, R2, ain_3));
+            // get adc readings
+            ain_1_avg.push(divider_vin(R1, R2, ain_1));
+            ain_2_avg.push(divider_vin(R1, R2, ain_2));
+            ain_3_avg.push(divider_vin(R1, R2, ain_3));
+
+            // wait for next tick
+            Mono::delay_until(start + 1_u64.millis()).await;
+        }
 
         // convert to j1939 slot
         let ain_1 = SaeEV06::from_f32(ain_1_avg.avg().unwrap()).unwrap();
@@ -82,8 +88,5 @@ pub async fn analog(cx: analog::Context<'_>) {
                 defmt::error!("Failed to build analog reading frame payload");
             }
         }
-
-        // wait until next tick
-        Mono::delay_until(start + 100_u64.millis()).await;
     }
 }
