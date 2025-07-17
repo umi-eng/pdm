59150409e87d8522c3a20aef9637dc4406d29490
Liam Kinne <liam@kinne.io>
2025-07-18 05:49:30 +0930

    refactor firmware update ready for testing

--- pdm-36/src/updater.rs
+++ pdm-36/src/updater.rs
@@ -1,30 +1,30 @@
 use crate::Mono;
+use crate::app::*;
 use crate::config::otp_slice;
 use crate::hal;
 use embassy_boot::FirmwareUpdaterError;
+use embassy_stm32::can::CanTx;
 use hal::can;
 use rtic_monotonics::systick::prelude::*;
-use saelient::Id;
+use rtic_sync::arbiter::Arbiter;
 use saelient::Pgn;
 use saelient::diagnostic::*;
 use saelient::transport::*;
-use vpd::otp;
-
-use crate::app::updater;
 
 pub async fn updater<'a>(cx: updater::Context<'_>) {
     let updater = cx.local.updater;
-    let can_tx = cx.shared.can_tx;
+    let can = cx.shared.can_tx;
     let source_address = *cx.shared.source_address;
 
-    let pubkey: otp::PubKey =
+    let pubkey: vpd::otp::PubKey =
         vpd::read_from_slice(&otp_slice()).expect("public key present in VPD");
 
     updater.mark_booted().await.unwrap();
 
-    let mut storage = [0; 1024];
     let mut offset = 0;
+    let mut storage = [0; 1024];
     let mut transfer: Option<Transfer<'_>> = None;
+    let mut firmware_size = 0;
 
     while let Ok(frame) = cx.local.updater_rx.recv().await {
         let id = match frame.id() {
@@ -35,102 +35,51 @@ pub async fn updater<'a>(cx: updater::Context<'_>) {
         match id.pgn() {
             Pgn::MemoryAccessRequest => {
                 if let Ok(req) = MemoryAccessRequest::try_from(frame.data()) {
-                    let response_id = saelient::Id::builder()
-                        .da(id.sa())
-                        .sa(source_address)
-                        .pgn(Pgn::MemoryAccessResponse)
-                        .priority(6)
-                        .build()
-                        .unwrap();
-
                     match req.command() {
+                        Command::Erase => {
+                            if let Pointer::Direct(_) = req.pointer() {
+                                updater.prepare_update().await.unwrap();
+                                firmware_size = 0;
+                            }
+                        }
                         Command::Write => {
                             if let Pointer::Direct(addr) = req.pointer() {
                                 offset = addr;
                                 transfer = None;
-
-                                let response = MemoryAccessResponse::new(
-                                    Status::Proceed,
-                                    ErrorIndicator::None,
-                                    req.length(),
-                                    0xFFFF,
-                                );
-
-                                can_tx
-                                    .access()
-                                    .await
-                                    .write(
-                                        &can::Frame::new_data(
-                                            response_id,
-                                            &<[u8; 8]>::from(&response),
-                                        )
-                                        .unwrap(),
-                                    )
-                                    .await;
+                                respond_proceed(can, source_address, id.sa(), req.length()).await;
                             } else {
-                                defmt::error!("Cannot handle spatial pointer.");
+                                defmt::warn!("Spatial data not handled yet.");
+                                respond_failed(
+                                    can,
+                                    source_address,
+                                    id.sa(),
+                                    ErrorIndicator::AddressingOutOfBounds,
+                                )
+                                .await;
                             }
                         }
                         Command::BootLoad => {
                             defmt::info!("Verifying firmware update.");
 
-                            let Some(last_transfer_len) = transfer
-                                .as_ref()
-                                .and_then(|f| f.finished().and_then(|f| Some(f.len())))
-                            else {
-                                let response = MemoryAccessResponse::new(
-                                    Status::OperationFailed,
-                                    ErrorIndicator::AddressingGeneral,
-                                    req.length(),
-                                    0xFFFF,
-                                );
-
-                                can_tx
-                                    .access()
-                                    .await
-                                    .write(
-                                        &can::Frame::new_data(
-                                            response_id,
-                                            &<[u8; 8]>::from(&response),
-                                        )
-                                        .unwrap(),
-                                    )
-                                    .await;
-                                continue;
-                            };
-
-                            let update_len = offset + last_transfer_len as u32;
+                            // read signature from end of firmware
                             let signature = &mut [0; 64];
-                            let sig_start = update_len - signature.len() as u32;
-
-                            updater.read_dfu(sig_start, signature).await.unwrap();
+                            let end_of_firmware = firmware_size - signature.len() as u32;
+                            updater.read_dfu(end_of_firmware, signature).await.unwrap();
 
                             match updater
-                                .verify_and_mark_updated(&pubkey.key, signature, update_len)
+                                .verify_and_mark_updated(&pubkey.key, signature, end_of_firmware)
                                 .await
                             {
                                 Ok(_) => {}
                                 Err(FirmwareUpdaterError::Signature(_)) => {
                                     defmt::warn!("Signature verification failed.");
-
-                                    let response = MemoryAccessResponse::new(
-                                        Status::OperationFailed,
+                                    respond_failed(
+                                        can,
+                                        source_address,
+                                        id.sa(),
                                         ErrorIndicator::Security,
-                                        req.length(),
-                                        0xFFFF,
-                                    );
-
-                                    can_tx
-                                        .access()
-                                        .await
-                                        .write(
-                                            &can::Frame::new_data(
-                                                response_id,
-                                                &<[u8; 8]>::from(&response),
-                                            )
-                                            .unwrap(),
-                                        )
-                                        .await;
+                                    )
+                                    .await;
                                     continue;
                                 }
                                 Err(err) => {
@@ -141,21 +90,7 @@ pub async fn updater<'a>(cx: updater::Context<'_>) {
                                 }
                             }
 
-                            let response = MemoryAccessResponse::new(
-                                Status::Proceed,
-                                ErrorIndicator::None,
-                                req.length(),
-                                0xFFFF,
-                            );
-
-                            can_tx
-                                .access()
-                                .await
-                                .write(
-                                    &can::Frame::new_data(response_id, &<[u8; 8]>::from(&response))
-                                        .unwrap(),
-                                )
-                                .await;
+                            respond_complete(can, source_address, id.sa(), req.length()).await;
 
                             defmt::info!("Booting into new firmware.");
                             Mono::delay(50_u64.millis()).await;
@@ -166,86 +101,57 @@ pub async fn updater<'a>(cx: updater::Context<'_>) {
                 }
             }
             Pgn::TransportProtocolConnectionManagement => {
+                let response_id = saelient::Id::builder()
+                    .sa(source_address)
+                    .da(id.sa())
+                    .pgn(Pgn::TransportProtocolConnectionManagement)
+                    .build()
+                    .unwrap();
+
                 if let Ok(rts) = RequestToSend::try_from(frame.data()) {
                     if rts.pgn() == Pgn::BinaryDataTransfer {
-                        let response_id = saelient::Id::builder()
-                            .sa(source_address)
-                            .da(id.sa())
-                            .pgn(Pgn::TransportProtocolConnectionManagement)
-                            .priority(6)
-                            .build()
-                            .unwrap();
                         let cts = ClearToSend::new(
                             rts.max_packets_per_response(),
                             0,
                             Pgn::BinaryDataTransfer,
                         );
-
-                        can_tx
-                            .access()
-                            .await
-                            .write(
-                                &can::Frame::new_data(response_id, &<[u8; 8]>::from(&cts)).unwrap(),
-                            )
-                            .await;
+                        let data: [u8; 8] = (&cts).into();
+                        let frame = can::Frame::new_data(response_id, &data).unwrap();
+                        can.access().await.write(&frame).await;
 
                         transfer = Some(Transfer::new_with_storage(rts, storage.as_mut_slice()));
                     } else {
                         defmt::warn!("Cannot start transfer for this pgn");
-                        continue;
+
+                        let abort = ConnectionAbort::new(
+                            AbortReason::UnexpectedDataTransfer,
+                            AbortSenderRole::Receiver,
+                            rts.pgn(),
+                        );
+                        let data: [u8; 8] = (&abort).into();
+                        let frame = can::Frame::new_data(response_id, &data).unwrap();
+                        can.access().await.write(&frame).await;
                     }
                 }
             }
             Pgn::TransportProtocolDataTransfer => {
+                let response_id = saelient::Id::builder()
+                    .sa(source_address)
+                    .da(id.sa())
+                    .pgn(Pgn::TransportProtocolConnectionManagement)
+                    .build()
+                    .unwrap();
+
                 if let Some(transfer) = &mut transfer {
                     if let Ok(dt) = DataTransfer::try_from(frame.data()) {
-                        let response_id = Id::builder()
-                            .pgn(Pgn::TransportProtocolConnectionManagement)
-                            .sa(source_address)
-                            .da(id.sa())
-                            .build()
-                            .unwrap();
-
                         match transfer.next(dt) {
                             Ok(Some(cts)) => {
-                                let frame =
-                                    can::Frame::new_data(response_id, &<[u8; 8]>::from(&cts))
-                                        .unwrap();
-                                can_tx.access().await.write(&frame).await;
+                                let data: [u8; 8] = (&cts).into();
+                                let frame = can::Frame::new_data(response_id, &data).unwrap();
+                                can.access().await.write(&frame).await;
 
                                 if let Response::End(_) = cts {
                                     defmt::info!("Writing firmware block.");
-                                    updater
-                                        .write_firmware(
-                                            offset as usize,
-                                            transfer.finished().unwrap(),
-                                        )
-                                        .await
-                                        .unwrap();
-
-                                    let response = MemoryAccessResponse::new(
-                                        Status::OperationCompleted,
-                                        ErrorIndicator::None,
-                                        transfer.finished().unwrap().len() as u16,
-                                        0xFFFF,
-                                    );
-                                    let response_id = saelient::Id::builder()
-                                        .pgn(Pgn::MemoryAccessResponse)
-                                        .sa(source_address)
-                                        .da(id.sa())
-                                        .build()
-                                        .unwrap();
-                                    can_tx
-                                        .access()
-                                        .await
-                                        .write(
-                                            &can::Frame::new_data(
-                                                response_id,
-                                                &<[u8; 8]>::from(&response),
-                                            )
-                                            .unwrap(),
-                                        )
-                                        .await;
                                 }
                             }
                             Ok(None) => {}
@@ -253,7 +159,7 @@ pub async fn updater<'a>(cx: updater::Context<'_>) {
                                 defmt::error!("Transfer aborted: {}", abort.reason());
                                 let data: [u8; 8] = (&abort).into();
                                 let frame = can::Frame::new_data(response_id, &data).unwrap();
-                                can_tx.access().await.write(&frame).await;
+                                can.access().await.write(&frame).await;
                             }
                         }
                     }
@@ -261,5 +167,50 @@ pub async fn updater<'a>(cx: updater::Context<'_>) {
             }
             _ => {}
         }
+
+        // when the transfer is finished we write the firmware to flash and
+        // respond accordingly.
+        if let Some(ongoing) = &mut transfer {
+            if let Some(data) = ongoing.finished() {
+                updater.write_firmware(offset as usize, data).await.unwrap();
+                respond_complete(can, source_address, id.sa(), data.len() as u16).await;
+                firmware_size += data.len() as u32;
+            }
+
+            // transfer finished
+            transfer = None;
+        }
     }
+}
+
+async fn respond<'a>(can: &Arbiter<CanTx<'a>>, sa: u8, da: u8, response: MemoryAccessResponse) {
+    let id = saelient::Id::builder()
+        .pgn(Pgn::MemoryAccessResponse)
+        .sa(sa)
+        .da(da)
+        .build()
+        .unwrap();
+    let data: [u8; 8] = (&response).into();
+    let frame = can::Frame::new_data(id, &data).unwrap();
+    can.access().await.write(&frame).await;
+}
+
+async fn respond_complete<'a>(can: &Arbiter<CanTx<'a>>, sa: u8, da: u8, len: u16) {
+    let response = MemoryAccessResponse::new(
+        Status::OperationCompleted,
+        ErrorIndicator::None,
+        len,
+        0xFFFF,
+    );
+    respond(can, sa, da, response).await
+}
+
+async fn respond_proceed<'a>(can: &Arbiter<CanTx<'a>>, sa: u8, da: u8, len: u16) {
+    let response = MemoryAccessResponse::new(Status::Proceed, ErrorIndicator::None, len, 0xFFFF);
+    respond(can, sa, da, response).await
+}
+
+async fn respond_failed<'a>(can: &Arbiter<CanTx<'a>>, sa: u8, da: u8, indicator: ErrorIndicator) {
+    let response = MemoryAccessResponse::new(Status::OperationFailed, indicator, 0x0, 0xFFFF);
+    respond(can, sa, da, response).await
 }
