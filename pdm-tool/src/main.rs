815d99c756d368159930bf201d1d8b49f522ec0a
Liam Kinne <liam@kinne.io>
2025-09-29 19:26:04 +1000

    allow hex addresses to be used

--- pdm-tool/src/main.rs
+++ pdm-tool/src/main.rs
@@ -11,7 +11,7 @@ struct Cli {
     #[clap(long)]
     interface: String,
     /// Device address.
-    #[clap(long, default_value_t = 0x55)]
+    #[clap(long, default_value_t = 0x55, value_parser=maybe_hex)]
     address: u8,
 
     #[clap(subcommand)]
@@ -51,4 +51,14 @@ async fn main() -> Result<()> {
 fn open_pdm(interface: &str, address: u8) -> Result<pdm36::Pdm36> {
     let socket = CanSocket::open(interface)?;
     Ok(pdm36::Pdm36::new(socket, address))
+}
+
+fn maybe_hex(input: &str) -> Result<u8, String> {
+    let result = if input.starts_with("0x") {
+        u8::from_str_radix(&input[2..], 16)
+    } else {
+        u8::from_str_radix(input, 10)
+    };
+
+    result.map_err(|e| format!("{e}"))
 }
